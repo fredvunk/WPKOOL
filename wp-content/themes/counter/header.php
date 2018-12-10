eb40e8fd1667d7e6ae098a800318847a7f78efb2
fredvunkiktkhk <fred.vunk@khk.ee>
2018-12-10 08:57:23 +0200

    modified theme `Counter` version 1.3.1

--- wp-content/themes/counter/header.php
+++ wp-content/themes/counter/header.php
@@ -13,6 +13,16 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130571351-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-130571351-1');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
